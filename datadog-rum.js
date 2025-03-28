8d4c2a7ee8715afec2dd3a5e93d40fbf197e02b6
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2025-03-28 17:20:37 +0000

    datadog

--- datadog-rum.js
+++ datadog-rum.js
@@ -2,8 +2,8 @@ import { datadogRum } from '@datadog/browser-rum';
 import { reactPlugin } from '@datadog/browser-rum-react';
 
 datadogRum.init({
-    applicationId: '09851315-8157-4a00-ab3b-23207296061d',
-    clientToken: 'pub573f8c19ef3b5cf0952f1215877534ae',
+    applicationId: '989f983d-f936-4be0-93a0-51ab58de4780',
+    clientToken: 'pubef38c55f19f3f9b29c0e7b6a070ab2ad',
     site: 'us5.datadoghq.com',
     service:'tictactoe',
     env: 'staging',
