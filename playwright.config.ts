44276f561148f05a4e8d1950d7b892521d0c2c00
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2025-04-09 13:49:21 +0000

    e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,11 +69,11 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://127.0.0.1:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+  // /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120 * 1000,
+  // },
 });
